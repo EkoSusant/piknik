4752b4735debb3ce9faad8aa7321873672842e0e
EkoSusant <ekcos38@yahoo.com>
2023-09-11 16:30:21 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/** @format */
+
+let navbar = document.querySelector(".header .flex .navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+// number
+document.querySelectorAll('input[type="number"]').forEach((inputNumber) => {
+  inputNumber.oninput = () => {
+    if (inputNumber.value.length > inputNumber.maxLength)
+      inputNumber.value = inputNumber.value.slice(0, inputNumber.maxLength);
+  };
+});
+
+//Aos
+AOS.init({
+  duration: 500,
+  delay: 200,
+});
